a6840b726263b3e9971e7d45df5063375f34cd04
Chris Valean <cvalean@cloudbasesolutions.com>
2015-06-25 15:24:53 +0300

    Update multi-provision.py

--- examples/multi-provision.py
+++ examples/multi-provision.py
@@ -44,7 +44,7 @@ def get_args():
     parser.add_argument('-M', '--monitoring', required=False, help='Enable monitoring on the virtual machines', dest='monitoring', action='store_true')
     parser.add_argument('-B', '--backend-name', required=False, help='The name of the backend to use for the provisioning. Defaults to the first available backend', dest='backend_name', type=str)
     parser.add_argument('-I', '--image-id', required=True, help='The image to deploy', dest='image_id')
-    parser.add_argument('-T', '--tenant-id', required=True, help='Tenant under which to create the image', dest='tenant_id')
+    parser.add_argument('-T', '--tenant-id', required=True, help='Tenant under which to create the image', dest='tenantId')
     parser.add_argument('-S', '--size-id', required=True, help='The id of the size/flavor to use', dest='size_id')
     parser.add_argument('-N', '--networks_id', required=False, help='Network IDs to add to instance', dest='networks')
     parser.add_argument('-s', '--post-script', nargs=1, required=False, help='Script to be called after each VM is created and booted.', dest='post_script', type=str)
@@ -69,7 +69,7 @@ def main():
 
     basename    = args.basename[0]
     image_id    = args.image_id
-    tenant_id    = args.tenant_id
+    tenantId    = args.tenantId
     size_id     = args.size_id
     networks     = args.networks
 
@@ -154,7 +154,7 @@ def main():
         res = backend.create_machine(name=basename,
                                      key=client.keys(search=basename)[0],
                                      image_id=image_id,
-                                     tenant_id=tenant_id,
+                                     tenantId=tenantId,
                                      location_id=backend.locations[0]['id'],
                                      size_id=size_id,
                                      networks=networks,
